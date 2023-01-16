8c4bd4391a9f9a1d376da1a10efb2d074f081dcd
kharsh <kharsh@teqmavens.com>
2023-01-16 12:18:58 +0530

    change my file

--- cake_php/src/Controller/UsersController.php
+++ cake_php/src/Controller/UsersController.php
@@ -61,15 +61,7 @@ class UsersController extends AppController
             if (!$user->getErrors) {
 
                 $image = $this->request->getData('image_file');
-                print_r('image');
-                print_r('image');
-                print_r('image');
-                print_r('image');
-                print_r('image');
-                print_r('image');
-                print_r('image');
-                print_r('image');
-
+              
                 $name = $image->getClientFilename();
                                                                                                                  
                 $targetPath = WWW_ROOT . 'img' . DS . $name;
